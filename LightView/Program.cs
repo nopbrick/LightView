f6753ab7c49af7d8fa8d7d24f9e8aca40864563c
Filip Ceglik <fceglik@gmail.com>
2021-12-27 16:10:06 +0100

    Get Basic info about environment

---
+++ LightView/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightView
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            if (args[0].ToUpper() == "GETBASICS")
+            {
+                Basics.GetBasics();
+            }
+            else if (args[0].ToUpper() == "GETDOMAIN" && args.Length > 1)
+            {
+                Basics.GetDomain(args[1]);
+            }
+        }
+    }
+}
