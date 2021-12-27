1522669de673e5b4d7d508863660bf837d9031e7
Filip Ceglik <fceglik@gmail.com>
2021-12-27 17:16:34 +0100

    Improve command line arguments parsing, search users across domains.

--- LightView/Program.cs
+++ LightView/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CommandLine;
 
 namespace LightView
 {
@@ -10,23 +11,47 @@ namespace LightView
     {
         static void Main(string[] args)
         {
-            
-            if (args[0].ToUpper() == "GETBASICS")
-            {
-                Basics.GetBasics();
-            }
-            else if (args[0].ToUpper() == "GETDOMAIN" && args.Length > 1)
-            {
-                Basics.GetDomain(args[1]);
-            }
-            else if (args[0].ToUpper() == "GETDOMAINUSER" && args.Length == 1)
-            {
-                Users.GetDomainUser();
-            }
-            else if (args[0].ToUpper() == "GETDOMAINUSER" && args.Length > 1)
-            {
-                Users.GetDomainUser(args[1]);
-            }
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed<Options>(o =>
+                {
+                    if (args[0].ToUpper() == "GETBASICS")
+                    {
+                        Basics.GetBasics();
+                    }
+                    else if (args[0].ToUpper() == "GETDOMAIN" && args.Length > 1)
+                    {
+                        Basics.GetDomain(args[1]);
+                    }
+                    else if (args[0].ToUpper() == "GETDOMAINUSERS")
+                    {
+                        if (o.Server != null)
+                        {
+                            Users.GetDomainUsers(o.Server);
+                        }
+                        else
+                        {
+                            Users.GetDomainUsers();
+                        }
+                    }
+                    else if (args[0].ToUpper() == "GETDOMAINUSER" && args.Length > 1)
+                    {
+                        if (o.Server != null)
+                        {
+                            Users.GetDomainUser(args[1], o.Server);
+                        }
+                        else
+                        {
+                            Users.GetDomainUser(args[1]);
+                        }
+                        
+                    }
+                });
+        }
+
+        public class Options
+        {
+            [Option('s', "server", Required=false, HelpText = "Specify Active Directory server for queries. ")]
+            public string Server { get; set; }
         }
     }
 }
--- LightView/Users.cs
+++ LightView/Users.cs
@@ -11,9 +11,26 @@ namespace LightView
     internal class Users
     {
         
-        public static void GetDomainUser()
+        public static void GetDomainUsers()
         {
             using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain))
+            {
+                UserPrincipal userPrincipal = new UserPrincipal(principalContext);
+                userPrincipal.Name = "*";
+
+                using (PrincipalSearcher principalSearcher = new PrincipalSearcher(userPrincipal))
+                {
+                    foreach (var result in principalSearcher.FindAll())
+                    {
+                        Console.WriteLine(result.SamAccountName);
+                    }
+                }
+            }
+        }
+
+        public static void GetDomainUsers(string domainServer)
+        {
+            using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, domainServer))
             {
                 UserPrincipal userPrincipal = new UserPrincipal(principalContext);
                 userPrincipal.Name = "*";
@@ -31,6 +48,24 @@ namespace LightView
         public static void GetDomainUser(string accountName)
         {
             using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain))
+            {
+                UserPrincipal userPrincipal = new UserPrincipal(principalContext);
+                userPrincipal.SamAccountName = '*' + accountName + '*';
+
+                using (PrincipalSearcher principalSearcher = new PrincipalSearcher(userPrincipal))
+                {
+                    foreach (var result in principalSearcher.FindAll())
+                    {
+                        Console.WriteLine(result.SamAccountName);
+                    }
+                }
+            }
+        }
+
+
+        public static void GetDomainUser(string accountName, string domainServer)
+        {
+            using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain, domainServer))
             {
                 UserPrincipal userPrincipal = new UserPrincipal(principalContext);
                 userPrincipal.SamAccountName = '*' + accountName + '*';
