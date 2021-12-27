26b4a88567d33d614b09d553c538fd0f5964fed8
Filip Ceglik <fceglik@gmail.com>
2021-12-27 16:45:15 +0100

    Searching users in current domain

---
+++ LightView/Users.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.DirectoryServices.ActiveDirectory;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices.AccountManagement;
+
+namespace LightView
+{
+    internal class Users
+    {
+        
+        static void GetDomainUser()
+        {
+            using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain))
+            {
+                UserPrincipal userPrincipal = new UserPrincipal(principalContext);
+                userPrincipal.Name = "*";
+
+                using (PrincipalSearcher principalSearcher = new PrincipalSearcher(userPrincipal))
+                {
+                    foreach (var result in principalSearcher.FindAll())
+                    {
+                        Console.WriteLine(result.SamAccountName);
+                    }
+                }
+            }
+        }
+
+        static void GetDomainUser(string accountName)
+        {
+            using (PrincipalContext principalContext = new PrincipalContext(ContextType.Domain))
+            {
+                UserPrincipal userPrincipal = new UserPrincipal(principalContext);
+                userPrincipal.Name = accountName;
+
+                using (PrincipalSearcher principalSearcher = new PrincipalSearcher(userPrincipal))
+                {
+                    foreach (var result in principalSearcher.FindAll())
+                    {
+                        Console.WriteLine(result.SamAccountName);
+                    }
+                }
+            }
+        }
+    }
+}
